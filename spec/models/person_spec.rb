08de87b4406e3f4fc8f3a9a401af72b92b343b61
David Maurer <davemaurermail@gmail.com>
2016-09-10 22:21:56 -0600

    Add test confirming person created by let clause is a valid object.

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -3,6 +3,10 @@ require 'rails_helper'
 RSpec.describe Person, type: :model do
   let(:person) { Person.new(first_name: 'Luke', last_name: 'Skywalker') }
 
+  it 'is valid' do
+    expect(person).to be_valid
+  end
+
   it 'is invalid without a first name' do
     person.first_name = nil
 
