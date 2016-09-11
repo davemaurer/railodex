792da991b7a8c8d9a0a793851fdccaf165582041
David Maurer <davemaurermail@gmail.com>
2016-09-10 22:13:21 -0600

    Add tests for validations on Person model. Add validates method to make them green.

--- spec/controllers/people_controller_spec.rb
+++ spec/controllers/people_controller_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe PeopleController, type: :controller do
   }
 
   let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
+    { first_name: nil, last_name: nil }
   }
 
   let(:valid_session) { {} }
@@ -79,14 +79,16 @@ RSpec.describe PeopleController, type: :controller do
   describe "PUT #update" do
     context "with valid params" do
       let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
+        { first_name: 'Joe', last_name: 'Dirt' }
       }
 
       it "updates the requested person" do
         person = Person.create! valid_attributes
         put :update, params: {id: person.to_param, person: new_attributes}, session: valid_session
         person.reload
-        skip("Add assertions for updated state")
+
+        expect(person.first_name).to eq('Joe')
+        expect(person.last_name).to eq('Dirt')
       end
 
       it "assigns the requested person as @person" do
--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Person, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is invalid without a first name' do
+    person = Person.new(first_name: 'Luke')
+
+    expect(person).not_to be_valid
+  end
+
+  it 'is invalid without a first name' do
+    person = Person.new(last_name: 'Skywalker')
+
+    expect(person).not_to be_valid
+  end
 end
