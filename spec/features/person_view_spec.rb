641e97772ed60ed86771ad16312ab220ad8b2ba1
David Maurer <davemaurermail@gmail.com>
2016-09-12 00:19:56 -0600

    Add test for email addresses displaying on show view for people. Make it
    pass by adding the html to the view.

--- spec/features/person_view_spec.rb
+++ spec/features/person_view_spec.rb
@@ -3,62 +3,76 @@ require 'rails_helper'
 describe 'this person view', type: :feature do
   let(:person) { Person.create(first_name: 'Mr', last_name: 'Rogers') }
 
-  before(:each) do
-    person.phone_numbers.create(number: '555-5555')
-    person.phone_numbers.create(number: '555-8888')
-    visit person_path(person)
-  end
+  describe 'phone numbers' do
+    before(:each) do
+      person.phone_numbers.create(number: '555-5555')
+      person.phone_numbers.create(number: '555-8888')
+      visit person_path(person)
+    end
 
-  it 'shows the phone numbers' do
-    person.phone_numbers.each do |phone|
-      expect(page).to have_content(phone.number)
+    it 'shows the phone numbers' do
+      person.phone_numbers.each do |phone|
+        expect(page).to have_content(phone.number)
+      end
+    end
+
+    it 'has a link to add a new phone number' do
+      expect(page).to have_link('Add new phone number', href: new_phone_number_path(person_id: person.id))
+    end
+
+    it 'adds a new phone number' do
+      page.click_link('Add new phone number')
+      page.fill_in('Number', with: '555-4321')
+      page.click_button('Create Phone number')
+
+      expect(current_path).to eq(person_path(person))
+      expect(page).to have_content('555-4321')
+    end
+
+    it 'has an edit link for each phone number' do
+      person.phone_numbers.each do |phone|
+        expect(page).to have_link('edit', href: edit_phone_number_path(phone))
+      end
+    end
+
+    it 'edits a phone number' do
+      phone = person.phone_numbers.first
+      old_number = phone.number
+
+      first(:link, 'edit').click
+      page.fill_in('Number', with: '555-9999')
+      page.click_button('Update Phone number')
+
+      expect(current_path).to eq(person_path(person))
+      expect(page).to have_content('555-9999')
+      expect(page).to_not have_content(old_number)
+    end
+
+    it 'has a delete link for each phone number' do
+      person.phone_numbers.each do |phone|
+        expect(page).to have_link('delete', href: phone_number_path(phone))
+      end
+    end
+
+    it 'can delete a phone number' do
+      expect(page).to have_content('555-5555')
+
+      first(:link, 'delete').click
+
+      expect(current_path).to eq(person_path(person))
+      expect(page).not_to have_content('555-5555')
     end
   end
 
-  it 'has a link to add a new phone number' do
-    expect(page).to have_link('Add new phone number', href: new_phone_number_path(person_id: person.id))
-  end
-
-  it 'adds a new phone number' do
-    page.click_link('Add new phone number')
-    page.fill_in('Number', with: '555-4321')
-    page.click_button('Create Phone number')
-
-    expect(current_path).to eq(person_path(person))
-    expect(page).to have_content('555-4321')
-  end
-
-  it 'has an edit link for each phone number' do
-    person.phone_numbers.each do |phone|
-      expect(page).to have_link('edit', href: edit_phone_number_path(phone))
+  describe 'email addresses' do
+    before(:each) do
+      person.email_addresses.create(address: 'batman@batman.com')
+      person.email_addresses.create(address: 'robin@robin.com')
+      visit person_path(person)
     end
-  end
 
-  it 'edits a phone number' do
-    phone = person.phone_numbers.first
-    old_number = phone.number
-
-    first(:link, 'edit').click
-    page.fill_in('Number', with: '555-9999')
-    page.click_button('Update Phone number')
-
-    expect(current_path).to eq(person_path(person))
-    expect(page).to have_content('555-9999')
-    expect(page).to_not have_content(old_number)
-  end
-
-  it 'has a delete link for each phone number' do
-    person.phone_numbers.each do |phone|
-      expect(page).to have_link('delete', href: phone_number_path(phone))
+    it 'has li tags for each email address' do
+      expect(page).to have_selector('li', text: 'batman@batman.com')
     end
-  end
-
-  it 'can delete a phone number' do
-    expect(page).to have_content('555-5555')
-
-    first(:link, 'delete').click
-
-    expect(current_path).to eq(person_path(person))
-    expect(page).not_to have_content('555-5555')
   end
 end
