688233b9c57190ae0ffe9528299c91d90414856a
David Maurer <davemaurermail@gmail.com>
2016-09-17 23:21:48 -0600

    Correct typo in OmniAuth inside omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
-Rails.application.config.middleware.use Omniauth::Builder do
+Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, 'ho5P5C0PhJTL3NIXy8bzdxzyQ', 'pnnSsw7f961dwEVtBD3BZVROfGPeehegXNcnvrQxVYMkWBj3O0'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get '/auth/:provider/callback', to: 'sessions#create'
+
   resources :companies
   resources :email_addresses, except: [:index, :show]
   resources :phone_numbers, except: [:index, :show]
