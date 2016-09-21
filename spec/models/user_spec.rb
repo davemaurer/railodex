3a0dc63b2409bae7bb1abb530bb532a09ec4a11c
David Maurer <davemaurermail@gmail.com>
2016-09-20 23:27:30 -0600

    Add test for a user being able to access people. Add association to user model.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { User.new }
+  let(:person_one) { Person.new(first_name: 'Bob', last_name: 'Smith') }
+
+  it 'has associated people' do
+    user.people.push(person_one)
+    expect(user.people.length).to eq(1)
+  end
 end
