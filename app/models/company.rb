852e3b2df6195c862d03a9f6e9eabc08ad7baa80
David Maurer <davemaurermail@gmail.com>
2016-09-22 23:58:24 -0600

    Add migration for user_id column on companies. Associate companies to user model.

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,7 +2,7 @@ class Company < ApplicationRecord
   include Contact
 
   belongs_to :user, required: false
-  
+
   validates :name, presence: true
 
   def to_s
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160923052038) do
+ActiveRecord::Schema.define(version: 20160923055523) do
 
   create_table "companies", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "email_addresses", force: :cascade do |t|
