dac41a2fcbfe1bf72b2c89b3ee0c395357109da3
David Maurer <davemaurermail@gmail.com>
2016-09-22 23:48:09 -0600

    Add test for user associating people

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,4 +8,14 @@ RSpec.describe User, type: :model do
     user.people.push(person_one)
     expect(user.people.length).to eq(1)
   end
+
+  it 'builds associated people' do
+    person_1 = Fabricate(:person)
+    person_2 = Fabricate(:person)
+    [person_1, person_2].each do |person|
+      expect(user.people).not_to include(person)
+      user.people << person
+      expect(user.people).to include(person)
+    end
+  end
 end
